55c005000322e64028b20b277f43de5f1abd4810
christianLizama <ckill661@gmail.com>
2024-05-22 21:33:41 -0400

    Corrección pequeña en vista

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomeScreen(),
+      home: WiFiScreen(),
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class WiFiScreen extends StatefulWidget {
+  const WiFiScreen({super.key});
 
   @override
-  createState() => _HomeScreenState();
+  createState() => _WiFiScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _WiFiScreenState extends State<WiFiScreen> {
   final TextEditingController ssidController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   String esp32Ip = "192.168.4.1"; // IP por defecto del ESP32
@@ -37,40 +37,15 @@ class _HomeScreenState extends State<HomeScreen> {
     );
 
     if (response.statusCode == 200) {
+      if (!mounted) return;
       print('Datos enviados exitosamente!');
-      // Obtén la nueva IP del ESP32
-      // await obtenerNuevaIp();
+      // Navegar a la pantalla de controles
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => ControlScreen(esp32Ip: esp32Ip)),
+      );
     } else {
       print('Error al enviar datos: ${response.statusCode}');
-    }
-  }
-
-  Future<void> obtenerNuevaIp() async {
-    final url = Uri.parse('http://192.168.4.1/get_ip');
-    final response = await http.get(url);
-
-    if (response.statusCode == 200) {
-      setState(() {
-        esp32Ip = response.body;
-        print('Nueva IP del ESP32: $esp32Ip');
-      });
-    } else {
-      print('Error al obtener nueva IP: ${response.statusCode}');
-    }
-  }
-
-  Future<void> enviarComando(String action) async {
-    final url = Uri.parse('http://192.168.226.203/move');
-    final response = await http.post(
-      url,
-      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-      body: 'action=$action',
-    );
-
-    if (response.statusCode == 200) {
-      print('Comando enviado exitosamente!');
-    } else {
-      print('Error al enviar comando: ${response.statusCode}');
     }
   }
 
@@ -78,7 +53,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Control del Auto'),
+        title: const Text('Configurar WiFi'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -100,58 +75,102 @@ class _HomeScreenState extends State<HomeScreen> {
               },
               child: const Text('Conectar a WiFi'),
             ),
-            const SizedBox(height: 20),
-            Table(
-              children: [
-                TableRow(
-                  children: [
-                    const SizedBox(), // Espacio vacío
-                    ElevatedButton(
-                      onPressed: () {
-                        enviarComando('backward');
-                      },
-                      child: const Text('Adelante'),
-                    ),
-                    const SizedBox(), // Espacio vacío
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        enviarComando('left');
-                      },
-                      child: const Text('Izquierda'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        enviarComando('stop');
-                      },
-                      child: const Text('Detenerse'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        enviarComando('right');
-                      },
-                      child: const Text('Derecha'),
-                    ),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    const SizedBox(), // Espacio vacío
-                    ElevatedButton(
-                      onPressed: () {
-                        enviarComando('forward');
-                      },
-                      child: const Text('Atras'),
-                    ),
-                    const SizedBox(), // Espacio vacío
-                  ],
-                ),
-              ],
-            ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class ControlScreen extends StatefulWidget {
+  final String esp32Ip;
+
+  const ControlScreen({super.key, required this.esp32Ip});
+
+  @override
+  createState() => _ControlScreenState();
+}
+
+class _ControlScreenState extends State<ControlScreen> {
+  Future<void> enviarComando(String action) async {
+    final url = Uri.parse('http://192.168.226.203/move'); // Reemplaza con la IP de tu ESP32
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      body: 'action=$action',
+    );
+
+    if (response.statusCode == 200) {
+      print('Comando enviado exitosamente!');
+    } else {
+      print('Error al enviar comando: ${response.statusCode}');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Control del Auto'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Table(
+                children: [
+                  TableRow(
+                    children: [
+                      const SizedBox(), // Espacio vacío
+                      ElevatedButton(
+                        onPressed: () {
+                          enviarComando('backward');
+                        },
+                        child: const Text('Adelante'),
+                      ),
+                      const SizedBox(), // Espacio vacío
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          enviarComando('left');
+                        },
+                        child: const Text('Izquierda'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          enviarComando('stop');
+                        },
+                        child: const Text('Detenerse'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          enviarComando('right');
+                        },
+                        child: const Text('Derecha'),
+                      ),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      const SizedBox(), // Espacio vacío
+                      ElevatedButton(
+                        onPressed: () {
+                          enviarComando('forward');
+                        },
+                        child: const Text('Atras'),
+                      ),
+                      const SizedBox(), // Espacio vacío
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
